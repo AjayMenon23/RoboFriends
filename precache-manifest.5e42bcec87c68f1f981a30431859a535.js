75516e181cecc8601eba348bb8840f77fe872450
Ajay Menon <ajayam23@gmail.com>
2020-06-23 20:49:23 +0530

    Updates

---
+++ precache-manifest.5e42bcec87c68f1f981a30431859a535.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113f47b9522eb124d77c7a472b9cfef8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f84756b83de11123999a",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "04ceb0e9c3d891c26f1a",
+    "url": "/static/css/main.8bb0facf.chunk.css"
+  },
+  {
+    "revision": "f84756b83de11123999a",
+    "url": "/static/js/2.3c2e190b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3c2e190b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04ceb0e9c3d891c26f1a",
+    "url": "/static/js/main.a86d2bbe.chunk.js"
+  },
+  {
+    "revision": "8c542a9580f0b42d1c03",
+    "url": "/static/js/runtime-main.8fcf26b9.js"
+  },
+  {
+    "revision": "460377b16bff3fa907647acd8732d141",
+    "url": "/static/media/FeltTipRomRegular.460377b1.woff"
+  }
+]);
